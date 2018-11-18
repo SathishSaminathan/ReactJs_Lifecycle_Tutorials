22f76ecb0456ddc17fb0641aac5c7a478e57ac5e
SathishSaminathan <Sathishcse1975@gmail.com>
2018-11-18 11:15:46 +0530

    initial_commit

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,13 +1,28 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 
-const Profile = (props) =>{
-    console.log(props)
+const Profile = props => {
+  const redir = () => {
     return (
-        <Link to={{
-            pathname:`${props.match.url}/posts` // Getting Dynamic link using template string
-        }}>Here is the link to /profile/posts</Link>
+      <Redirect to="/posts" /> 
+      /**"<Redirect to="/posts" />" will Redirecting to the posts page 
+       * or we can use props.history.push("/posts") so we can less the components
+      */
     );
-}
+  };
+  console.log(props);
+  return (
+    <div>
+      <Link
+        to={{
+          pathname: `${props.match.url}/posts` // Getting Dynamic link using template string
+        }}
+      >
+        Here is the link to /profile/posts
+      </Link>
+      {redir()}
+    </div>
+  );
+};
 
 export default Profile;
